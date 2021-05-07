3d1f8d2273c94677822af8d5da28d067e62e0264
黎贵奇 <carney520@hotmail.com>
2021-05-07 16:10:52 +0800

    feat: 多分包间接依赖优化

--- src/index.ts
+++ src/index.ts
@@ -10,33 +10,45 @@ import InjectSubpackageRequirePlugin from './inject-require-plugin'
  * @param subPackages
  * @returns
  */
-function generateCacheGroups(subPackages: SubPackageInfo[], priority: number) {
-  // 缓存模块的引用情况，即是否只在子包中被引用
-  const cache = new Map<string, boolean>()
+function generateCacheGroups(subPackages: SubPackageInfo[], priority: number, enableCSS: boolean) {
+  return subPackages.reduce<{ [key: string]: any }>((prev, subPackage) => {
+    // 缓存模块的引用情况，即是否只在子包中被引用
+    const cache = new Map<string, boolean>()
 
-  function isUnderSubpackage(subPackage: SubPackageInfo, reasons: Set<Module>) {
-    for (const reason of reasons) {
-      // 即不再子包下面，间接依赖也不再子包下面
-      if (!reason.context?.startsWith(subPackage.path) && !cache.get(getModuleUniqId(reason))) {
-        return false
+    /**
+     * 判断是否在分包下面
+     * @param subPackage
+     * @param reasons
+     * @returns
+     */
+    function isUnderSubpackage(reasons: Set<Module>) {
+      for (const reason of reasons) {
+        // 即不再子包下面，间接依赖也不再子包下面
+        if (!reason.context?.startsWith(subPackage.path) && !cache.get(getModuleUniqId(reason))) {
+          return false
+        }
       }
+
+      return true
     }
 
-    return true
-  }
-
-  return subPackages.reduce<{ [key: string]: any }>((prev, subPackage) => {
     prev[subPackage.name] = {
       name: subPackage.chunkName,
       minChunks: 2,
       // 默认优先级高于 common, vendor
       priority,
       enforce: true,
-      // 如果该模块完全被子包引用，那么就应该放到子包中
+      /**
+       * 如果该模块完全被子包引用，那么就应该放到子包中
+       */
       test(module: Module & { reasons?: Array<{ module: Module }> }) {
         // Taro 入口、页面、配置不能放到共享 chunk 中
-        // @ts-ignore
-        if (module.miniType != null || module.context?.includes('@tarojs') || module.type !== 'javascript/auto') {
+        if (
+          // @ts-ignore
+          module.miniType != null ||
+          module.context?.includes('@tarojs') ||
+          (!enableCSS && module.type !== 'javascript/auto')
+        ) {
           return false
         }
 
@@ -48,7 +60,7 @@ function generateCacheGroups(subPackages: SubPackageInfo[], priority: number) {
           // 去掉重复的 module
           const reasons = new Set(module.reasons.map((i) => i.module))
 
-          if (reasons.size > 0 && isUnderSubpackage(subPackage, reasons)) {
+          if (reasons.size > 0 && isUnderSubpackage(reasons)) {
             cache.set(id, true)
             return true
           }
@@ -63,10 +75,10 @@ function generateCacheGroups(subPackages: SubPackageInfo[], priority: number) {
 
 export default function TaroPluginSubpackageOptimize(
   ctx: IPluginContext,
-  options: { enableInDev?: boolean; priority?: number } = {}
+  options: { enableInDev?: boolean; priority?: number; enableCSS?: boolean } = {}
 ) {
   // 默认优先级高于 vendor 和 common
-  options = { enableInDev: false, priority: 20, ...options }
+  options = { enableInDev: false, priority: 20, enableCSS: true, ...options }
 
   // 只适用于小程序平台
   if (!['weapp', 'swan', 'alipay', 'tt', 'qq', 'jd'].includes(process.env.TARO_ENV!)) {
@@ -89,7 +101,7 @@ export default function TaroPluginSubpackageOptimize(
     }
 
     const splitChunksConfig = chain.optimization.get('splitChunks') || {}
-    const cacheGroups = generateCacheGroups(subPackages, options.priority!)
+    const cacheGroups = generateCacheGroups(subPackages, options.priority!, options.enableCSS!)
 
     // 扩展 split chunk
     chain.optimization.splitChunks({
--- src/inject-require-plugin.ts
+++ src/inject-require-plugin.ts
@@ -1,26 +1,26 @@
-import { ConcatSource } from 'webpack-sources';
-import { WebpackPluginInstance, Compiler, Chunk } from 'webpack';
-import { SubPackageInfo, addRequireToSource, getIdOrName, SHARE_CHUNK_NAME } from './utils';
+import { ConcatSource } from 'webpack-sources'
+import { WebpackPluginInstance, Compiler, Chunk } from 'webpack'
+import { SubPackageInfo, addRequireToSource, getIdOrName, SHARE_CHUNK_NAME } from './utils'
 
-const PLUGIN_NAME = 'inject-subpackage-require';
+const PLUGIN_NAME = 'inject-subpackage-require'
 
 export default class InjectSubpackageRequirePlugin implements WebpackPluginInstance {
-  static NAME = PLUGIN_NAME;
-  private subPackageRoots: SubPackageInfo[];
+  static NAME = PLUGIN_NAME
+  private subPackageRoots: SubPackageInfo[]
   constructor(subPackageRoots: SubPackageInfo[]) {
-    this.subPackageRoots = subPackageRoots;
+    this.subPackageRoots = subPackageRoots
   }
 
   apply(compile: Compiler) {
     compile.hooks.thisCompilation.tap(PLUGIN_NAME, (comp) => {
       // 有共享 chunk 的分包
-      let subpackageHasShareChunks: SubPackageInfo[];
+      let subpackageHasShareChunks: SubPackageInfo[]
       comp.hooks.afterOptimizeChunks.tap(PLUGIN_NAME, (chunks: Iterable<Chunk>) => {
-        const commonChunks = Array.from(chunks).filter((c) => c.name.includes(SHARE_CHUNK_NAME));
+        const commonChunks = Array.from(chunks).filter((c) => c.name.includes(SHARE_CHUNK_NAME))
         subpackageHasShareChunks = this.subPackageRoots.filter((pkg) => {
-          return commonChunks.some((c) => getIdOrName(c) === pkg.chunkName);
-        });
-      });
+          return commonChunks.some((c) => getIdOrName(c) === pkg.chunkName)
+        })
+      })
 
       // 给分包每个页面添加上 require
       comp.chunkTemplate.hooks.renderWithEntry.tap(PLUGIN_NAME, (modules: ConcatSource, chunk: Chunk) => {
@@ -28,11 +28,11 @@ export default class InjectSubpackageRequirePlugin implements WebpackPluginInsta
           for (const pkg of subpackageHasShareChunks) {
             if (chunk.entryModule && chunk.entryModule.context?.startsWith(pkg.path)) {
               // 添加 require
-              return addRequireToSource(getIdOrName(chunk), modules, pkg.chunkName);
+              return addRequireToSource(getIdOrName(chunk), modules, pkg.chunkName)
             }
           }
         }
-      });
-    });
+      })
+    })
   }
 }
